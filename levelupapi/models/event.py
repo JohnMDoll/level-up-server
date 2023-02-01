6ca7eb5e575b5de0d1403e3cd3416e0b842783dc
JohnMDoll <johnmdoll3@gmail.com>
2023-02-01 10:31:33 -0600

    initial migrations

---
+++ levelupapi/models/event.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Event(models.Model):
+
+    description = models.CharField(max_length=500)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    attendees = models.ManyToManyField("Gamer", related_name="events_attending" )
+    organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name="organizer_events" )
+    game = models.ForeignKey("Game", on_delete=models.CASCADE, related_name="game_events")
