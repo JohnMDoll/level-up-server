2e355b5cf4b5f0e63da4ff58855a873b79816f88
JohnMDoll <johnmdoll3@gmail.com>
2023-02-22 11:04:01 -0600

    finished core requests

--- levelupreports/views/__init__.py
+++ levelupreports/views/__init__.py
@@ -1,1 +1,3 @@
 from .users.games_by_user import UserGameList
+from .users.events_by_user import UserEventList
+
--- levelupreports/views/users/events_by_user.py
+++ levelupreports/views/users/events_by_user.py
@@ -6,26 +6,25 @@ from django.views import View
 from levelupreports.views.helpers import dict_fetch_all
 from operator import itemgetter
 
-class UserGameList(View):
+class UserEventList(View):
     def get(self, request):
         with connection.cursor() as db_cursor:
 
             # TODO: Write a query to get all games along with the gamer first name, last name, and id
             db_cursor.execute("""
                 SELECT
-                    g.id,
-                    g.name,
-                    g.maker,
-                    g.game_type_id,
-                    g.number_of_players,
-                    g.skill_level,
-                    g.gamer_id,
-                    u.id user_id,
+                    e.id,
+                    e.description,
+                    e.date,
+                    e.organizer_id,
+                    g.name AS game_name,
                     u.first_name || " " || u.last_name AS full_name
                 FROM
-                    levelupapi_game g
+                    levelupapi_event e
                 JOIN
-                    levelupapi_gamer gmr ON g.gamer_id = gmr.id
+                    levelupapi_game g ON g.id = e.game_id
+                JOIN
+                    levelupapi_gamer gmr ON gmr.id = e.organizer_id
                 JOIN
                     auth_user u ON gmr.user_id = u.id
             """)
@@ -34,77 +33,65 @@ class UserGameList(View):
 
             # Take the flat data from the dataset, and build the
             # following data structure for each gamer.
-            # This will be the structure of the games_by_user list:
+            # This will be the structure of the events_by_user list:
             #
             # [
             #   {
-            #     "id": 1,
-            #     "full_name": "Admina Straytor",
-            #     "games": [
+            #     "gamer_id": 1,
+            #     "full_name": "Molly Ringwald",
+            #     "events": [
             #       {
-            #         "id": 1,
-            #         "title": "Foo",
-            #         "maker": "Bar Games",
-            #         "skill_level": 3,
-            #         "number_of_players": 4,
-            #         "game_type_id": 2
-            #       },
-            #       {
-            #         "id": 2,
-            #         "title": "Foo 2",
-            #         "maker": "Bar Games 2",
-            #         "skill_level": 3,
-            #         "number_of_players": 4,
-            #         "game_type_id": 2
+            #         "id": 5,
+            #         "date": "2020-12-23",
+            #         "time": "19:00",
+            #         "game_name": "Fortress America"
             #       }
             #     ]
-            #   },
+            #   }
             # ]
 
-            games_by_user = []
+            events_by_user = []
 
             for row in dataset:
                 # TODO: Create a dictionary called game that includes 
                 # the name, description, number_of_players, maker,
                 # game_type_id, and skill_level from the row dictionary
-                game = {
+                event = {
                     'id': row['id'],
-                    'name': row['name'],
-                    # 'maker': row['maker'],
-                    # 'skill_level': row['skill_level'],
-                    # 'number_of_players': row['number_of_players'],
-                    # 'game_type_id': row['game_type_id']
+                    'description': row['description'],
+                    'date': row['date'],
+                    'game_name': row['game_name']
                 }
                 
                 # See if the gamer has been added to the games_by_user list already
                 user_dict = None
-                for user_game in games_by_user:
-                    if user_game['gamer_id'] == row['gamer_id']:
-                        user_dict = user_game
+                for user_event in events_by_user:
+                    if user_event['gamer_id'] == row['organizer_id']:
+                        user_dict = user_event
                 
                 
                 if user_dict:
                     # If the user_dict is already in the games_by_user list, append the game to the games list
-                    user_dict['games'].append(game)
+                    user_dict['events'].append(event)
                 else:
                     # If the user is not on the games_by_user list, create and add the user to the list
-                    games_by_user.append({
-                        "gamer_id": row['gamer_id'],
+                    events_by_user.append({
+                        "gamer_id": row['organizer_id'],
                         "full_name": row['full_name'],
-                        "games": [game]
+                        "events": [event]
                     })
             #this orders the results from highest gamer id to lowest since reverse=True outside of x definition. Here to test ability to reorder list.
-            games_by_user = sorted(games_by_user, key=lambda x: x['gamer_id'], reverse=True)
+            events_by_user = sorted(events_by_user, key=lambda x: x['gamer_id'], reverse=True)
             # how to sort by games within list without a for loop?
-            for user in games_by_user:
-                user['games'] = sorted(user['games'], key=lambda x: x['id'], reverse=True)
+            for user in events_by_user:
+                user['events'] = sorted(user['events'], key=lambda x: x['date'], reverse=True)
 
         # The template string must match the file name of the html template
-        template = 'users/list_with_games.html'
+        template = 'users/list_with_events.html'
         
         # The context will be a dictionary that the template can access to show data
         context = {
-            "usergame_list": games_by_user
+            "userevent_list": events_by_user
         }
 
         return render(request, template, context)
